d4f48787b1233c03964a43ecfd98d57e926ab708
AtaberkGorgen <ataberk.goregen@gmail.com>
2026-04-17 00:36:24 +0300

    CORS ayarı eklendi

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // 2. Sonra ayarları (middleware) yapıyoruz
-app.use(cors()); // CORS her zaman en üstte olmalı
+app.use(
+  cors({
+    origin: "https://is-takip-frontend-jet.vercel.app", // "*" yerine tam adresini yazmak her zaman daha garantidir
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 // 3. Sonra rotaları (routes) tanımlıyoruz
